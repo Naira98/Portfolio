7b28c1f120b59c4d2fb78bee4e92811a14a84198
Naira98 <nairamm99@gmail.com>
2024-11-02 18:15:11 +0200

    adding hero part

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main className="realtive mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
+    <main className="bg-black-100 relative mx-auto flex h-screen flex-col items-center justify-center overflow-hidden px-5 sm:px-10">
       <div className="w-full max-w-7xl">
-        <h1>Hello, Portfolio</h1>
+        <Hero />
       </div>
     </main>
   );
