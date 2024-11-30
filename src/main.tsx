691c748cec060d7c26478e8093be8a9ae6349bb8
Naira98 <nairamm99@gmail.com>
2024-11-30 15:42:22 +0200

    enable Sentry in production only

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,8 @@ Sentry.init({
   dsn: "https://933f970a055d21d706b4de342ce713bc@o4508360462761984.ingest.de.sentry.io/4508382357356624",
   integrations: [Sentry.browserTracingIntegration()],
 
+  enabled: process.env.NODE_ENV == "production",
+
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
